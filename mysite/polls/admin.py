9861f09b944a5d1fd828702352cad23d7a795152
NeiraJ <johnny.neira@gmail.com>
2024-10-07 14:16:46 +0200

    Amélioration de l'interface admin pour les modèles Question et Choice
    - Création de classes QuestionAdmin et ChoiceAdmin personnalisées
    - Configuration des champs à afficher dans la liste (list_display)
    - Ajout de filtres (list_filter) et de champs de recherche (search_fields)
    - Enregistrement des modèles personnalisés auprès de l'interface admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'votes')
+    list_filter = ['question']
+    search_fields = ['choice_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
