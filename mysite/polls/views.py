01f1555bf1d2ec59b40b624986ad6fc8e8f29237
NeiraJ <johnny.neira@gmail.com>
2024-10-08 15:10:23 +0200

    Amélioration des vues pour gérer l'affichage de tous les sondages
    - Ajout d'une nouvelle vue "all" pour afficher tous les sondages
    - Utilisation de la même logique que la vue index pour récupérer les questions
    - Rendu du template "all.html" avec les données des questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,6 +10,13 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 
+def all(request):
+    latest_question_list = Question.objects.order_by("-pub_date")
+
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/all.html", context)
+
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
