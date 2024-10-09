eb456f77ebe66cd601902cd670e74c1ef739e733
NeiraJ <johnny.neira@gmail.com>
2024-10-09 13:14:09 +0200

    Mise à jour des chemins URL pour inclure la nouvelle page de statistiques
    - Renommage du chemin pour la liste des questions par date de publication
    - Ajout d'un nouveau chemin pour la page de statistiques

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('all/', views.all, name='all'),
+    path('all/', views.all_questions_by_pub_date, name='all'),
     path('<int:question_id>/frequency/', views.frequency, name='frequency'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('statistics/', views.statistics, name='statistics'),
 ]
