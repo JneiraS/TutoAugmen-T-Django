778f65bfb1512655578723096036e8e41c3ba89b
NeiraJ <johnny.neira@gmail.com>
2024-10-09 14:39:25 +0200

    Nouveau formulaire : Création de QuestionForm pour gérer les questions

---
+++ mysite/polls/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import Question, Choice
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['question_text']
+        widgets = {
+            'question_text': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
