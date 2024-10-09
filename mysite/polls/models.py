b2b0d29d415ef2844bd88d486fb175fb7b2fd6ce
NeiraJ <johnny.neira@gmail.com>
2024-10-09 08:58:25 +0200

    Amélioration des modèles Question et Choice
    - Modification de la méthode get_choices() pour inclure les pourcentages de votes
    - Ajout d'une nouvelle méthode get_sum_votes() pour calculer la somme totale des votes
    - Mise à jour des annotations de type pour refléter les changements

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Any
 
 from django.db import models
 from django.utils import timezone
@@ -19,10 +20,18 @@ class Question(models.Model):
         """Retourne l'âge de la question en jours."""
         return (timezone.now() - self.pub_date).days
 
-    def get_choices(self) -> list[tuple[str, int]]:
-        """Retourne la liste des choix de la question ainsi que leur nombre de votes."""
+    def get_sum_votes(self):
+        """Retourne la somme des votes de la question."""
+        return sum(choice.votes for choice in self.choice_set.all())
+
+    def get_choices(self) -> list[tuple[Any, Any, float | int | Any]]:
+        """
+        Retourne la liste des choix de la question ainsi que leur nombre de votes et leur proportions
+        :return:
+        """
         choices = self.choice_set.all()
-        return [(choice.choice_text, choice.votes) for choice in choices]
+        sum_votes = self.get_sum_votes() if self.get_sum_votes() > 0 else 1
+        return [(choice.choice_text, choice.votes, choice.votes / sum_votes * 100) for choice in choices]
 
     def get_max_choice(self):
         """Retourne le choix avec le plus de votes."""
