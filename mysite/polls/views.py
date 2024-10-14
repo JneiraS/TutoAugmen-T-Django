e5194e5ea2fb99bddfb7c8c89d0befafa9fdd6df
NeiraJ <johnny.neira@gmail.com>
2024-10-14 08:47:48 +0200

    Amélioration : Ajout de la décoration @login_required pour la vue de création de nouveaux sondages

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
@@ -55,6 +56,7 @@ def statistics(request):
     return render(request, "polls/statistics.html", context)
 
 
+@login_required(login_url="/login/")
 def new_poll(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
