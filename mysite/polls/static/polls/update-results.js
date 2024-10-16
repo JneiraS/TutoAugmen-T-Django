9991047c9e4bff049d9b93273070063e5e0a3182
NeiraJ <johnny.neira@gmail.com>
2024-10-16 09:49:09 +0200

    Amélioration : Optimisation de la gestion des messages WebSocket pour la mise à jour des votes

--- mysite/polls/static/polls/update-results.js
+++ mysite/polls/static/polls/update-results.js
@@ -1,34 +1,26 @@
 // Client WebSocket sur la page statistics
-const wsStatistics = new WebSocket('ws://localhost:8080');
-
-
-wsStatistics.onopen = function() {
-  console.log('Connected to WebSocket server on statistics page');
-};
-
-wsStatistics.onmessage = function(event) {
+const wsStatistics = new WebSocket("ws://localhost:8080");
+// Fonction appelée lorsque la connexion au serveur WebSocket est établie.
+wsStatistics.onopen = function () {};
+// Fonction appelée lorsque le serveur envoie un message.
+wsStatistics.onmessage = function (event) {
   // Vérifie si le message est un Blob
   if (event.data instanceof Blob) {
     const reader = new FileReader();
 
     // Quand la lecture est terminée, le contenu est dans reader.result
-    reader.onload = function() {
+    reader.onload = function () {
       const message = reader.result; // Contenu du Blob converti en texte
-      console.log('Message from server on statistics page (b): ', message);
-      const nbVotes = parseInt(document.querySelector(`.nb_votes${message}`).innerHTML);
-      document.querySelector(`.nb_votes${message}`).innerHTML = nbVotes+1;
-
+      const nbVotes = parseInt(
+        document.querySelector(`.nb_votes${message}`).innerHTML
+      );
+      document.querySelector(`.nb_votes${message}`).innerHTML = nbVotes + 1;
     };
     // Lire le Blob en tant que texte
     reader.readAsText(event.data);
   } else {
     // Si le message n'est pas un Blob, le traiter directement comme texte
     const message = event.data;
-    console.log('Message from server on statistics page (nb): ', message);
     document.querySelector(`.nb_votes${message}`).innerHTML = message;
-
-
-    // Si le message est "refresh-statistics", on rafraîchit la page
   }
-
 };
