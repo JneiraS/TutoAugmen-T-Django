4ff4c972a74bf09afb19c32976253bb7326695ed
NeiraJ <johnny.neira@gmail.com>
2024-10-17 13:19:42 +0200

    feat:  affichage basique des notifications

--- mysite/polls/static/polls/ws.js
+++ mysite/polls/static/polls/ws.js
@@ -1,24 +1,25 @@
-// Connexion au serveur WebSocket
-const wsSubmit = new WebSocket("ws://localhost:8080");
+// Client WebSocket sur la page statistics
+const wsStatistics = new WebSocket("ws://92.134.222.0:8089");
 // Fonction appelée lorsque la connexion au serveur WebSocket est établie.
-wsSubmit.onopen = function () {};
+wsStatistics.onopen = function () {};
 // Fonction appelée lorsque le serveur envoie un message.
-wsSubmit.onmessage = function (event) {};
-// Écouteur d'événement, quand le contenu du DOM est chargé.
-document.addEventListener("DOMContentLoaded", function () {
-  //L'élément de formulaire à écouter pour les événements de soumission.
-  const form = document.querySelector("#post-form");
+wsStatistics.onmessage = function (event) {
+  // Vérifie si le message est un Blob
+  if (event.data instanceof Blob) {
+    const reader = new FileReader();
 
-  if (form) {
-    form.addEventListener("submit", function (event) {
-      // Envoyer un message au serveur WebSocket
-      if (wsSubmit.readyState === WebSocket.OPEN) {
-        wsSubmit.send("Form submitted"); // Message pour notifier le serveur
-      } else {
-        console.error("WebSocket is not open. Cannot send message.");
-      }
-    });
-  } else {
-    console.error("Le formulaire #post-form n'a pas été trouvé dans le DOM.");
+
+
+
+    // Quand la lecture est terminée, le contenu est dans reader.result
+    reader.onload = function () {
+      const message = reader.result; // Contenu du Blob converti en texte
+       const actualNotification = parseInt(document.querySelector(`.notification`).innerHTML
+      );
+      document.querySelector(`.notification`).innerHTML += "<li> Notifications: " + message  + "</li>";
+    };
+
+    // Lire le Blob en tant que texte
+    reader.readAsText(event.data);
   }
-});
+};
